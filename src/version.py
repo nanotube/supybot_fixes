eefec89518925e6c240f2f42dd8cf2f9de59dfee
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2014-12-21 23:25:09 -0500

    update version timestamp

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2013-06-28T14:41:32-0400)'
+version = '0.83.4.1+gribble (2014-12-21T23:25:09-0500)'
